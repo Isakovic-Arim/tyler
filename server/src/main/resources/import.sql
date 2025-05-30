555cca6f8c00960b4e3f5276e5d59c39b0228e46
Isakovic-Arim <arim.isakovic2005@gmail.com>
2025-05-30 19:32:21 +0200

    Apply initial priorities using flyway migration to ensure they are persisted on first ever startup

--- server/src/main/resources/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO priority (id, name, xp) VALUES (1, 'Low', 1);
-INSERT INTO priority (id, name, xp) VALUES (2, 'Medium', 2);
-INSERT INTO priority (id, name, xp) VALUES (3, 'High', 3);
-INSERT INTO priority (id, name, xp) VALUES (4, 'Critical', 5);
